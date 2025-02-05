8444fe05b2b3dd48d007c25fc7b348d1e7e7dfe6
rajan <surajguptaaaa93@gmail.com>
2025-02-06 00:53:09 +0530

    Resolved merge conflicts

---
+++ product/src/main/java/blinket/com/product/exception/InvalidProductDetails.java
@@ -0,0 +1,7 @@
+package blinket.com.product.exception;
+
+public class Invalidproductdetails extends RuntimeException {
+  public Invalidproductdetails(String message) {
+    super(message);
+  }
+}
---
+++ product/src/main/java/blinket/com/product/exception/ProductNotFoundException.java
@@ -0,0 +1,8 @@
+package blinket.com.product.exception;
+
+public class ProductException extends RuntimeException {
+    public ProductException(String message) {
+
+        super();
+    }
+}
